266a7c2d071b7f8f3ba287f759c63041ac43a267
Priyanshu-8058 <goswamipriyanshu86@gmail.com>
2025-06-29 13:44:52 +0530

    Add files via upload

---
+++ musicLibrary.py
@@ -0,0 +1,8 @@
+music = {
+    "chulha" : "https://youtu.be/j67MMW8h9Ic?si=Wx_NeVugeEB_JzNm",
+    "raat" : "https://youtu.be/xMSuYh4ElPs?si=mpGPeBpmS6QuqBuC",
+    "blender" : "https://youtu.be/gGnGSKKsATM?si=c6PWEc3vGQFaogJY",
+    "Khatole" : "https://youtu.be/obgMGM6I2rE?si=Jpm_VxJxxzW53hTb",
+    "ramayan ka saar" : "https://youtu.be/szUSkYTXq7A?si=smP_ZEgkF9ks_an-",
+
+}
